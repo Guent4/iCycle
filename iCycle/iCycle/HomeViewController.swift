77beb6b16250c9596b3a65a7daa489f5c3987599
Huajun Gu <davidgu96@gmail.com>
2016-09-17 17:49:05 -0400

    added view controllers for all pages; added Charts

---
+++ iCycle/iCycle/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  
+//
+//  Created by David Gu on 9/17/16.
+//  Copyright © 2016 GSR. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Charts
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
