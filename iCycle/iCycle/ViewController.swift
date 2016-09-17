06103b9815407ede984066f1fba3a820880dfa0a
David Gu <davidgu@Davids-Mac.local>
2016-09-17 01:39:54 -0700

    setup workspace

--- iCycle/iCycle/ViewController.swift
+++ iCycle/iCycle/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+//import
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+//    func testDatabase() {
+//        let con = MySQL.Connection();
+//        let db_name = "iCycle";
+//        
+//        do {
+//            try con.open("52.165.33.228", user: "root", passwd: "password");
+//            
+//            let select_stmt = try con.prepare("SELECT * FROM User;");
+//            
+//            let res = try select_stmt.query([i]);
+//        }
+//        catch (let e) {
+//            print(e)
+//        }
+        
+//    }
 }
 
