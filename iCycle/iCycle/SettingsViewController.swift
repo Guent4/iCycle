7f1fe0ad32f84e24442a06367b0017ffa913bac2
Gautham Sezhian <gauthamsezhian@Gauthams-Mac.local>
2016-09-17 23:51:55 -0700

    fixed keyboard stick, updated settings storyboard

--- iCycle/iCycle/SettingsViewController.swift
+++ iCycle/iCycle/SettingsViewController.swift
@@ -11,11 +11,27 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    @IBOutlet weak var oldPassword: UITextField!
+    @IBOutlet weak var newPassword: UITextField!
+    
+    @IBOutlet weak var newPassConfirm: UITextField!
+    
+    @IBAction func changePassword(sender: AnyObject) {
+    }
+    @IBOutlet weak var numHistory: UISegmentedControl!
+    
+    @IBOutlet weak var numFriends: UISegmentedControl!
+    @IBOutlet weak var updateSettings: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
     }
     
     override func didReceiveMemoryWarning() {
