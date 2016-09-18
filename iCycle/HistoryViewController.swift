8932d2ec7f3e9bd6ef50ad4b3c285acc6c512eed
Gautham Sezhian <gauthamsezhian@Gauthams-Mac.local>
2016-09-17 17:46:59 -0700

    changed text to labels, added two classes

---
+++ iCycle/HistoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HistoryViewController.swift
+//  
+//
+//  Created by Gautham Sezhian on 9/17/16.
+//
+//
+
+import Foundation
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+}
