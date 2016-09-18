db006461fc1506cca8409b227e066c1864140a01
Gautham Sezhian <gauthamsezhian@Gauthams-Mac.local>
2016-09-18 02:40:50 -0700

    added Chart in Home

--- iCycle/iCycle/BarChartViewController.swift
+++ iCycle/iCycle/BarChartViewController.swift
@@ -12,18 +12,47 @@ import Charts
 
 class BarChartViewController: UIViewController {
     
+    var interval: [String]!
+    var viewType: Int!
+    var unitsSold: [Double]!
+    
     @IBOutlet weak var barChartView: BarChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        barChartView.noDataText = "You need to provide data for the chart."
+        if (viewType == 0) {
+            interval = ["0000-0259", "0300-0559", "0600-0859", "0900-1159", "1200-1459", "1500-1759", "1800-2059", "2100-2359"]
+            unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0]
+        } else if (viewType == 1) {
+            interval = ["Day - 6", "Day - 5", "Day - 4", "Day - 3", "Day - 2", "Day - 1", "Today"]
+            unitsSold = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
+        } else {
+            interval = ["3 Wks Ago", "2 Wks Ago", "Last Week", "This Week"]
+            unitsSold = [20.0, 4.0, 6.0, 3.0]
+        }
+        setChart(interval, values: unitsSold)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        barChartView.noDataText = "You need to provide data for the chart."
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let chartData = BarChartData(xVals: interval, dataSet: chartDataSet)
+        barChartView.data = chartData
+        
     }
     
 }
--- iCycle/iCycle/HomeViewController.swift
+++ iCycle/iCycle/HomeViewController.swift
@@ -12,9 +12,14 @@ import Charts
 
 class HomeViewController: UIViewController {
     
+    var toPass: Int = 0
+    var destVC: BarChartViewController!
+    
+    @IBOutlet weak var chartScope: UISegmentedControl!
     @IBOutlet weak var TimeSinceLastRecycled: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        toPass = chartScope.selectedSegmentIndex
         // let mostRecent = DataService.retrieveMostRecentItem()[0]
         // TimeSinceLastRecycled.text = mostRecent["Name"] as? String
         // DataService.retrieveItemsForDay()
@@ -22,6 +27,19 @@ class HomeViewController: UIViewController {
         // DataService.retrieveItemsForMonth()
         // Do any additional setup after loading the view, typically from a nib.
         print(UserID);
+    }
+    @IBAction func onChartChange(sender: AnyObject) {
+        toPass = chartScope.selectedSegmentIndex
+        destVC.viewType = toPass
+        destVC.viewDidLoad()
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let embeddedVC = segue.destinationViewController as? BarChartViewController where segue.identifier == "EmbedSegue" {
+            embeddedVC.viewType = toPass
+        }
+        destVC = segue.destinationViewController as! BarChartViewController
     }
     
     override func didReceiveMemoryWarning() {
