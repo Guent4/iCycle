36d674b846330e9ec47b3751581e3ac7be418412
Huajun Gu <davidgu96@gmail.com>
2016-09-17 22:27:16 -0400

    fixed login button functionality

--- iCycle/iCycle/LoginViewController.swift
+++ iCycle/iCycle/LoginViewController.swift
@@ -17,6 +17,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var UsernameText: UITextField!
     @IBOutlet weak var PasswordText: UITextField!
     @IBOutlet weak var LoginButton: UIButton!
+    @IBOutlet weak var PasswordIncorrectLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +47,7 @@ class LoginViewController: UIViewController {
             let select_stmt = try con.prepare("SELECT * FROM User WHERE (Username=?) AND (Password=?);");
             
             let res = try select_stmt.query([username, password]);
-            var rows	 = try res.readAllRows();
+            var rows = try res.readAllRows();
             
             if (rows?.count > 0) {
                 var user = rows![0][0];
@@ -55,13 +56,12 @@ class LoginViewController: UIViewController {
                 // Go to home view
                 let next = self.storyboard!.instantiateViewControllerWithIdentifier("TabViewController") as UIViewController;
                 self.presentViewController(next, animated: true, completion: nil);
+                PasswordIncorrectLabel.hidden = true;
             } else {
-                print("failed");
+                PasswordText.text = "";
+                PasswordIncorrectLabel.hidden = false;
             }
-            
-            print(rows);
-        }
-        catch (let e) {
+        } catch (let e) {
             print(e)
         }
 
